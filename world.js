3cde1f5d722f3b856ab834b7d8ecfb206806e94a
Davaskye DeBique <Davaskye@gmail.com>
2021-11-05 18:55:43 -0400

    Made table dynamic. Added working city button.

--- world.js
+++ world.js
@@ -2,13 +2,22 @@ var httpRequest = new XMLHttpRequest();
 
 window.onload = function(){ 
 
-    var lookup_button = document.getElementById("lookup"); 
+    var lookup_button = document.getElementById("lookup");
+    var lookup_city = document.getElementById("lookup_cities"); 
 
     lookup_button.onclick = function(){
         console.log("Button works!");
         let searchVal = document.getElementById("country").value;
         httpRequest.onreadystatechange = updateResult;
         httpRequest.open('GET', "world.php?country=" + searchVal);
+        httpRequest.send();
+    }
+
+    lookup_city.onclick = function(){
+        console.log("Button works city!");
+        let searchVal = document.getElementById("country").value;
+        httpRequest.onreadystatechange = updateResult;
+        httpRequest.open('GET', "world.php?country=" + searchVal + "&context=cities");
         httpRequest.send();
     }
 }
--- world.php
+++ world.php
@@ -4,11 +4,25 @@ $username = 'lab5_user';
 $password = 'password123';
 $dbname = 'world';
 
-$country = $_GET['country'];
-//echo $country;
+$country = htmlspecialchars($_GET['country']);
+$context = "Nope";
+if(isset($_GET['context'])){
+  $context = htmlspecialchars($_GET['context']);
+}
 
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
-$stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
+
+if ($context == 'cities'){
+  $headers = ['Name', 'District', 'Population'];
+  $rows = ['name', 'district', 'population'];
+  $stmt = $conn->query("SELECT cities.name, cities.district, cities.population FROM cities JOIN countries ON cities.country_code = countries.code WHERE countries.name LIKE '%$country%'");
+}
+else{
+  $headers = ['Name', 'Continent', 'Independence Year', 'Head of State'];
+  $rows = ['name', 'continent', 'independence_year', 'head_of_state'];
+  $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
+}
+
 
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
@@ -17,17 +31,15 @@ $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 <table>
   <tr>
-    <th>Name</th>
-    <th>Continent</th>
-    <th>Independence</th>
-    <th>Head of State</th>
+    <?php foreach ($headers as $header): ?>
+    <th><?= $header; ?></th>
+    <?php endforeach; ?>
   </tr>
   <?php foreach ($results as $row): ?>
   <tr>
-    <td><?= $row['name']; ?></td>
-    <td><?= $row['continent']; ?> </td>
-    <td><?= $row['independence_year']; ?> </td>
-    <td><?= $row['head_of_state']; ?> </td>
+    <?php foreach ($rows as $filler): ?>
+    <td><?= $row[$filler]; ?></td>
+    <?php endforeach; ?>
   </tr>
-<?php endforeach; ?>
+  <?php endforeach; ?>
 </table>
